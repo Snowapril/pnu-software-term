dbff5dd162932892d7eade3428d91ff1b80e35b8
snowapril <sinjihng@naver.com>
2021-05-12 02:16:05 +0900

    feat : implement profile message ongoing

---
+++ app/src/main/java/com/software_term/gitpnu/fragments/ProfileFragment.java
@@ -0,0 +1,183 @@
+package com.software_term.gitpnu.fragments;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.software_term.gitpnu.R;
+import com.software_term.gitpnu.adapter.RepoAdapter;
+import com.software_term.gitpnu.api.GithubAPI;
+import com.software_term.gitpnu.api.GithubClient;
+import com.software_term.gitpnu.model.GithubRepo;
+import com.software_term.gitpnu.model.GithubUser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment {
+
+    private String m_token;
+    ImageView avatarImg;
+    TextView userNameTV;
+    TextView followersTV;
+    TextView followingTV;
+    TextView logIn;
+    TextView email;
+    Button ownedrepos;
+
+    Bundle extras;
+    String newString;
+
+    Bitmap myImage;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static ProfileFragment newInstance(String token) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString("token", token);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        m_token = getArguments().getString("token");
+        //loadData();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_home, container, false);
+        avatarImg = (ImageView)rootView.findViewById(R.id.avatar);
+        userNameTV = (TextView)rootView.findViewById(R.id.username);
+        followersTV = (TextView)rootView.findViewById(R.id.followers);
+        followingTV = (TextView)rootView.findViewById(R.id.following);
+        logIn = (TextView)rootView.findViewById(R.id.logIn);
+        email = (TextView)rootView.findViewById(R.id.email);
+
+        Log.e("Frag", "MainFragment");
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_home, menu);
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+            return null;
+        }
+    }
+
+    public void loadData(){
+        GithubClient apiService =
+                GithubAPI.getClient().create(GithubClient.class);
+
+        Call<GithubUser> call = apiService.getUser("snowapril");
+        call.enqueue(new Callback<GithubUser>() {
+            @Override
+            public void onResponse(Call<GithubUser> call, Response<GithubUser> response) {
+                ImageDownloader task = new ImageDownloader();
+
+                try {
+                    myImage = task.execute(response.body().getAvatar()).get();
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+
+                avatarImg.setImageBitmap(myImage);
+                avatarImg.getLayoutParams().height=220;
+                avatarImg.getLayoutParams().width=220;
+
+                if(response.body().getName() == null){
+                    userNameTV.setText("No name provided");
+                } else {
+                    userNameTV.setText("Username: " + response.body().getName());
+                }
+
+                followersTV.setText("Followers: " + response.body().getFollowers());
+                followingTV.setText("Following: " + response.body().getFollowing());
+                logIn.setText("LogIn: " + response.body().getLogin());
+
+                if(response.body().getEmail() == null){
+                    email.setText("No email provided");
+                } else{
+                    email.setText("Email: " + response.body().getEmail());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GithubUser> call, Throwable t) {
+                // Log error here since request failed
+                Log.e("Repos", t.toString());
+            }
+
+        });
+    }
+}
