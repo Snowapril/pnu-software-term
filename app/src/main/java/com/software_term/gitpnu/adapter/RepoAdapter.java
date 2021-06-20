5fc285cce443d21b9e61a2f9ac42eddc27360597
snowapril <sinjihng@naver.com>
2021-06-20 20:06:08 +0900

    feat : implement repository list overview in home fragment
    need to align text

--- app/src/main/java/com/software_term/gitpnu/adapter/RepoAdapter.java
+++ app/src/main/java/com/software_term/gitpnu/adapter/RepoAdapter.java
@@ -2,75 +2,141 @@ package com.software_term.gitpnu.adapter;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.software_term.gitpnu.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.invoke.ConstantCallSite;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import com.software_term.gitpnu.model.GithubRepo;
+import com.software_term.gitpnu.windows.RepoActivity;
 
 public class RepoAdapter extends RecyclerView.Adapter<RepoAdapter.RepoViewHolder>  {
 
-    private List<GithubRepo> repos;
-    private int rowLayout;
-    private Context context;
+    private List<GithubRepo> m_repos;
+    private int m_rowLayout;
+    private Context m_context;
+    private OnNoteListener m_onNoteListener;
 
-    public RepoAdapter(List<GithubRepo> repos, int rowLayout, Context context) {
+    public RepoAdapter(List<GithubRepo> repos, int rowLayout, Context context, OnNoteListener onNoteListener) {
         this.setRepos(repos);
         this.setRowLayout(rowLayout);
         this.setContext(context);
+        this.m_onNoteListener = onNoteListener;
     }
 
-    public List<GithubRepo> getRepos() {return repos;}
+    public List<GithubRepo> getRepos() {return m_repos;}
 
-    public void setRepos(List<GithubRepo> repos) {this.repos = repos;}
+    public void setRepos(List<GithubRepo> repos) {this.m_repos = repos;}
 
-    public int getRowLayout() {return rowLayout;}
+    public int getRowLayout() {return m_rowLayout;}
 
-    public void setRowLayout(int rowLayout) {this.rowLayout = rowLayout;}
+    public void setRowLayout(int rowLayout) {this.m_rowLayout = rowLayout;}
 
-    public Context getContext() {return context;}
+    public Context getContext() {return m_context;}
 
-    public void setContext(Context context) {this.context = context;}
+    public void setContext(Context context) {this.m_context = context;}
 
-    public static class RepoViewHolder extends RecyclerView.ViewHolder {
+    public static class RepoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         LinearLayout reposLayout;
+        ImageView avatar;
         TextView repoName;
-        TextView repoDescription;
-        TextView repolanguage;
+        TextView repoURL;
+        TextView repoDate;
+        TextView repoID;
+        Bitmap avatarBitmap;
+        OnNoteListener onNoteListener;
 
-
-        public RepoViewHolder(View v) {
+        public RepoViewHolder(@NonNull View v, OnNoteListener onNoteListener) {
             super(v);
             reposLayout = (LinearLayout) v.findViewById(R.id.repo_item_layout);
-            repoName = (TextView) v.findViewById(R.id.repoName);
-            repoDescription = (TextView) v.findViewById(R.id.repoDescription);
-            repolanguage = (TextView) v.findViewById(R.id.repoLanguage);
+            avatar = (ImageView) v.findViewById(R.id.avatar_img);
+            repoName = (TextView) v.findViewById(R.id.repo_name);
+            repoURL = (TextView) v.findViewById(R.id.repo_url);
+            repoDate = (TextView) v.findViewById(R.id.repo_date);
+            repoID = (TextView) v.findViewById(R.id.repo_id);
+            this.onNoteListener = onNoteListener;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onNoteListener.onNoteClick(getAdapterPosition());
         }
     }
 
     @Override
     public RepoAdapter.RepoViewHolder onCreateViewHolder(ViewGroup parent,
                                                            int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout, parent, false);
-        return new RepoViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(m_rowLayout, parent, false);
+        return new RepoViewHolder(view, m_onNoteListener);
     }
 
+    private class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 
     @Override
     public void onBindViewHolder(RepoViewHolder holder, final int position) {
-        holder.repoName.setText(repos.get(position).getName());
-        holder.repoDescription.setText(repos.get(position).getDescription());
-        holder.repolanguage.setText(repos.get(position).getLanguage());
+
+        ImageDownloader task = new ImageDownloader();
+        try {
+            holder.avatarBitmap = task.execute(m_repos.get(position).getOwner().getAvatarUrl()).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        holder.avatar.setImageBitmap(holder.avatarBitmap);
+        holder.avatar.getLayoutParams().height=220;
+        holder.avatar.getLayoutParams().width=220;
+
+        holder.repoName.setText(m_repos.get(position).getName());
+        holder.repoName.setText(m_repos.get(position).getName());
+        holder.repoURL.setText(m_repos.get(position).getUrl());
+        holder.repoDate.setText(m_repos.get(position).getUpdatedDate());
+        holder.repoID.setText(String.format("%d", m_repos.get(position).getId()));
     }
 
     @Override
-    public int getItemCount() { return repos.size();}
+    public int getItemCount() { return m_repos.size();}
+
+    public interface OnNoteListener {
+        void onNoteClick(int position);
+    }
 }
--- app/src/main/java/com/software_term/gitpnu/fragments/HomeFragment.java
+++ app/src/main/java/com/software_term/gitpnu/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.software_term.gitpnu.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,12 +9,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 
 import com.software_term.gitpnu.R;
@@ -21,6 +24,7 @@ import com.software_term.gitpnu.adapter.RepoAdapter;
 import com.software_term.gitpnu.api.GithubAPI;
 import com.software_term.gitpnu.api.GithubClient;
 import com.software_term.gitpnu.model.GithubRepo;
+import com.software_term.gitpnu.windows.RepoActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,12 +38,12 @@ import retrofit2.Response;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements RepoAdapter.OnNoteListener {
 
     private String m_token;
     RecyclerView m_recyclerView;
     List<GithubRepo> m_datasource = new ArrayList<>();
-    RecyclerView.Adapter m_adapter;
+    RepoAdapter m_adapter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -64,16 +68,15 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("HomeFragment", "onCreateView");
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_home, container, false);
         m_recyclerView = (RecyclerView) rootView.findViewById(R.id.repos_recycler_view);
 
         m_recyclerView.setHasFixedSize(true);
-        m_adapter = new RepoAdapter(m_datasource, R.layout.repo_list_item,
-                getActivity());
+        m_adapter = new RepoAdapter(m_datasource, R.layout.repo_list_item, getActivity(), this);
         m_recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         m_recyclerView.setAdapter(m_adapter);
 
-        Log.e("Frag", "MainFragment");
         return rootView;
     }
 
@@ -84,14 +87,14 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_home, menu);
+        inflater.inflate(R.menu.menu_bottom_nav, menu);
     }
 
     public void loadRepositories(){
         GithubClient apiService =
                 GithubAPI.getClient().create(GithubClient.class);
 
-        Call<List<GithubRepo>> call = apiService.getReposForUser("snowapril");
+        Call<List<GithubRepo>> call = apiService.getRepos(String.format("token %s", m_token));
         call.enqueue(new Callback<List<GithubRepo>>() {
             @Override
             public void onResponse(Call<List<GithubRepo>> call, Response<List<GithubRepo>> response) {
@@ -105,7 +108,15 @@ public class HomeFragment extends Fragment {
                 // Log error here since request failed
                 Log.e("Repos", t.toString());
             }
-
         });
+    }
+
+    @Override
+    public void onNoteClick(int position) {
+        Intent intent = new Intent(getActivity(), RepoActivity.class);
+        intent.putExtra("token", m_token);
+        intent.putExtra("repo", m_datasource.get(position).getName());
+        intent.putExtra("login", m_datasource.get(position).getOwner().getLogin());
+        startActivity(intent);
     }
 }
