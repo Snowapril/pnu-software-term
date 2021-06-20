72b6866a47a05a963e6fce473c9e4a1cd4297461
snowapril <sinjihng@naver.com>
2021-06-21 01:19:37 +0900

    feat : add README file downloader & renderer with markwon dependency

--- app/src/main/java/com/software_term/gitpnu/windows/RepoActivity.java
+++ app/src/main/java/com/software_term/gitpnu/windows/RepoActivity.java
@@ -1,32 +1,43 @@
 package com.software_term.gitpnu.windows;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.software_term.gitpnu.R;
 import com.software_term.gitpnu.api.GithubAPI;
 import com.software_term.gitpnu.api.GithubClient;
+import com.software_term.gitpnu.api.ImageDownloader;
+import com.software_term.gitpnu.api.TextDownloader;
 import com.software_term.gitpnu.databinding.ActivityRepoBinding;
 import com.software_term.gitpnu.model.GithubRepo;
 import com.software_term.gitpnu.model.GithubRepoReadme;
+
+import org.commonmark.node.Node;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Set;
 
+import io.noties.markwon.Markwon;
+import io.noties.markwon.ext.tables.TablePlugin;
+import io.noties.markwon.image.ImagesPlugin;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
 
 public class RepoActivity extends AppCompatActivity {
 
@@ -46,32 +57,6 @@ public class RepoActivity extends AppCompatActivity {
         String repo = getIntent().getStringExtra("repo");
 
         loadRepo(login, repo);
-    }
-
-    private class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
-        @Override
-        protected Bitmap doInBackground(String... urls) {
-
-            try {
-
-                URL url = new URL(urls[0]);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-                InputStream inputStream = connection.getInputStream();
-                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
-                return myBitmap;
-
-            } catch (MalformedURLException e) {
-
-                e.printStackTrace();
-
-            } catch (IOException e) {
-
-                e.printStackTrace();
-
-            }
-            return null;
-        }
     }
 
     private void loadRepo(String username, String repo) {
@@ -105,6 +90,17 @@ public class RepoActivity extends AppCompatActivity {
                     m_binding.licenseType.setText(repoBody.getLicenseName());
                 }
                 loadReadme(username, repo);
+
+                m_binding.issue.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(RepoActivity.this, IssueActivity.class);
+                        intent.putExtra("token", m_token);
+                        intent.putExtra("owner", repoBody.getOwner().getLogin());
+                        intent.putExtra("repo", repoBody.getName());
+                        startActivity(intent);
+                    }
+                });
             }
 
             @Override
@@ -123,8 +119,24 @@ public class RepoActivity extends AppCompatActivity {
             public void onResponse(Call<GithubRepoReadme> call, Response<GithubRepoReadme> response) {
                 if (response.code() == 200)
                 {
-                    m_binding.repoReadme.setText(response.body().getDownloadUrl());
-                    Log.d("loadReadme", "api load done");
+                    String readmeContents = null;
+                    TextDownloader task = new TextDownloader();
+                    try {
+                        final Context context = getApplicationContext();
+                        readmeContents = task.execute(response.body().getDownloadUrl()).get();
+
+                        final Markwon markwon = Markwon.builder(context)
+                                .usePlugin(ImagesPlugin.create())
+                                .usePlugin(TablePlugin.create(context))
+                                .build();
+                        // parse markdown to commonmark-java Node
+                        final Node node = markwon.parse(readmeContents);
+                        final Spanned markdown = markwon.render(node);
+                        markwon.setParsedMarkdown(m_binding.repoReadme, markdown);
+                        Log.d("loadReadme", "api load done");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
 
