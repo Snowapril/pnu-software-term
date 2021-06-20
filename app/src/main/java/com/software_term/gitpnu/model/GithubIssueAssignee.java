680837fac819565a9b579283bc34af7e3d1c7452
snowapril <sinjihng@naver.com>
2021-06-21 01:20:21 +0900

    feat : add Retrofit2 data class for github issue assignees

---
+++ app/src/main/java/com/software_term/gitpnu/model/GithubIssueAssignee.java
@@ -0,0 +1,20 @@
+package com.software_term.gitpnu.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class GithubIssueAssignee {
+
+    @SerializedName("login")
+    private String login;
+
+    @SerializedName("avatar_url")
+    private String avatarUrl;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+}
