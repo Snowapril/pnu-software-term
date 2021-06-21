b5dbca3324f683d13f154bb1d78f894926142d49
snowapril <sinjihng@naver.com>
2021-06-21 18:31:19 +0900

    feat : implement user profile README viewer to profile fragment

--- app/src/main/java/com/software_term/gitpnu/fragments/ProfileFragment.java
+++ app/src/main/java/com/software_term/gitpnu/fragments/ProfileFragment.java
@@ -1,17 +1,23 @@
 package com.software_term.gitpnu.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Spanned;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -27,8 +33,12 @@ import com.software_term.gitpnu.adapter.RepoAdapter;
 import com.software_term.gitpnu.api.GithubAPI;
 import com.software_term.gitpnu.api.GithubClient;
 import com.software_term.gitpnu.api.ImageDownloader;
+import com.software_term.gitpnu.api.TextDownloader;
 import com.software_term.gitpnu.model.GithubRepo;
+import com.software_term.gitpnu.model.GithubRepoReadme;
 import com.software_term.gitpnu.model.GithubUser;
+
+import org.commonmark.node.Node;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -36,8 +46,17 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
+import io.noties.markwon.Markwon;
+import io.noties.markwon.ext.tables.TablePlugin;
+import io.noties.markwon.image.ImageItem;
+import io.noties.markwon.image.ImagesPlugin;
+import io.noties.markwon.image.SchemeHandler;
+import io.noties.markwon.image.data.DataUriSchemeHandler;
+import io.noties.markwon.image.file.FileSchemeHandler;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -56,10 +75,7 @@ public class ProfileFragment extends Fragment {
     TextView followingTV;
     TextView logIn;
     TextView email;
-    Button ownedrepos;
-
-    Bundle extras;
-    String newString;
+    TextView readme;
 
     Bitmap myImage;
 
@@ -94,6 +110,7 @@ public class ProfileFragment extends Fragment {
         followingTV = (TextView)rootView.findViewById(R.id.following);
         logIn = (TextView)rootView.findViewById(R.id.logIn);
         email = (TextView)rootView.findViewById(R.id.email);
+        readme = (TextView)rootView.findViewById(R.id.readme);
 
         return rootView;
     }
@@ -108,8 +125,7 @@ public class ProfileFragment extends Fragment {
     }
 
     private void loadData(){
-        GithubClient apiService =
-                GithubAPI.getClient().create(GithubClient.class);
+        GithubClient apiService = GithubAPI.getClient().create(GithubClient.class);
 
         Call<GithubUser> call = apiService.getAuthroizedUser(String.format("token %s", m_token));
         call.enqueue(new Callback<GithubUser>() {
@@ -127,8 +143,8 @@ public class ProfileFragment extends Fragment {
                 }
 
                 avatarImg.setImageBitmap(myImage);
-                avatarImg.getLayoutParams().height=220;
-                avatarImg.getLayoutParams().width=220;
+                avatarImg.getLayoutParams().height=380;
+                avatarImg.getLayoutParams().width=260;
 
                 if(response.body().getName() == null){
                     userNameTV.setText("No name provided");
@@ -145,6 +161,8 @@ public class ProfileFragment extends Fragment {
                 } else{
                     email.setText("Email: " + response.body().getEmail());
                 }
+
+                loadReadme(response.body().getLogin(), response.body().getLogin());
             }
 
             @Override
@@ -153,6 +171,74 @@ public class ProfileFragment extends Fragment {
                 Log.e("Repos", t.toString());
             }
 
+        });
+    }
+
+    private void loadReadme(String username, String repo){
+        GithubClient apiService = GithubAPI.getClient().create(GithubClient.class);
+        Call<GithubRepoReadme> repoCall = apiService.getRepoReadme(username, repo, String.format("token %s", m_token));
+        repoCall.enqueue(new Callback<GithubRepoReadme>() {
+            @Override
+            public void onResponse(Call<GithubRepoReadme> call, Response<GithubRepoReadme> response) {
+                if (response.code() == 200)
+                {
+                    String readmeContents = null;
+                    TextDownloader task = new TextDownloader();
+                    try {
+                        final Context context = getContext();
+                        readmeContents = task.execute(response.body().getDownloadUrl()).get();
+
+                        final Markwon markwon = Markwon.builder(context)
+                                .usePlugin(ImagesPlugin.create(new ImagesPlugin.ImagesConfigure() {
+                                    @Override
+                                    public void configureImages(@NonNull ImagesPlugin plugin) {
+                                        plugin.addSchemeHandler(DataUriSchemeHandler.create());
+                                        plugin.addSchemeHandler(FileSchemeHandler.createWithAssets(context));
+                                        // for example to return a drawable resource
+                                        plugin.addSchemeHandler(new SchemeHandler() {
+                                            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                                            @NonNull
+                                            @Override
+                                            public ImageItem handle(@NonNull String raw, @NonNull Uri uri) {
+
+                                                // will handle URLs like `drawable://ic_account_24dp_white`
+                                                final int resourceId = context.getResources().getIdentifier(
+                                                        raw.substring("drawable://".length()),
+                                                        "drawable",
+                                                        context.getPackageName());
+
+                                                // it's fine if it throws, async-image-loader will catch exception
+                                                final Drawable drawable = context.getDrawable(resourceId);
+
+                                                return ImageItem.withResult(drawable);
+                                            }
+
+                                            @NonNull
+                                            @Override
+                                            public Collection<String> supportedSchemes() {
+                                                return Collections.singleton("drawable");
+                                            }
+                                        });
+                                    }
+                                }))
+                                .usePlugin(TablePlugin.create(context))
+                                .build();
+                        // parse markdown to commonmark-java Node
+                        final Node node = markwon.parse(readmeContents);
+                        final Spanned markdown = markwon.render(node);
+                        markwon.setParsedMarkdown(readme, markdown);
+                        Log.d("loadReadme", "api load done");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GithubRepoReadme> call, Throwable t) {
+                // Log error here since request failed
+                Log.e("Repos", t.toString());
+            }
         });
     }
 }
